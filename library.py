43b7414f01b986a6f7b36dcf7ed3153386f6e157
GlavRab <alek.bar.ru@yandex.ru>
2021-03-01 19:19:42 +0300

    feat: define a func to get a random quote from file

--- library.py
+++ library.py
@@ -1,5 +1,7 @@
 from flask import request
 import requests
+import json
+import random
 
 
 url = 'https://api.telegram.org/1666792495:AAGiLEJHfILirO4oLnLfipH2avv4aa9rTv0'
@@ -20,3 +22,14 @@ def send_message(chat_id: int, text: str) -> None:
     """Отправка сообщения"""
     data = {'chat_id': chat_id, 'text': text}
     requests.post(url=url+'sendMessage', data=data)
+
+
+def get_a_quote() -> str:
+    """Function to get a random quote from a json file
+    """
+    filename = 'quotes.json'
+    with open(filename) as file:
+        file_with_quotes = json.load(file)
+        number_of_quote = random.randint(1, 7)
+        quote = file_with_quotes[number_of_quote]
+        return quote
