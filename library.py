f0fbe5153b77609da1e5a922b89e6b2b9ab1fd72
GlavRab <alek.bar.ru@yandex.ru>
2021-03-01 18:30:03 +0300

    feat: define a func to send a message to the user

--- library.py
+++ library.py
@@ -1,12 +1,22 @@
 from flask import request
+import requests
 
 
-def parsing_data():
+url = 'https://api.telegram.org/1666792495:AAGiLEJHfILirO4oLnLfipH2avv4aa9rTv0'
+
+
+def parsing_data() -> dict:
     """Парсинг сообщения"""
     message_info = request.get_json()
     message = message_info['message']
     chat_info = message['chat']
     chat_id = chat_info['chat_id']
     text = message['text']
-    data = {'text': text, 'chat_id': chat_id}
+    data = {'chat_id': chat_id, 'text': text}
     return data
+
+
+def send_message(chat_id: int, text: str) -> None:
+    """Отправка сообщения"""
+    data = {'chat_id': chat_id, 'text': text}
+    requests.post(url=url+'sendMessage', data=data)
