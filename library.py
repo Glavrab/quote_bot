bf5c7a8c9e6d280ebb84d1c28c3e5bc0f5f7e4f0
GlavRab <alek.bar.ru@yandex.ru>
2021-03-01 19:34:18 +0300

    refactor: change the docstrings style and make parsing_data works if there is no text in message

--- library.py
+++ library.py
@@ -4,24 +4,29 @@ import json
 import random
 
 
-url = 'https://api.telegram.org/1666792495:AAGiLEJHfILirO4oLnLfipH2avv4aa9rTv0'
+URL = 'https://api.telegram.org/1666792495:AAGiLEJHfILirO4oLnLfipH2avv4aa9rTv0'
 
 
 def parsing_data() -> dict:
-    """Парсинг сообщения"""
+    """Function to parse a data from user to get a chat id and define text
+    """
     message_info = request.get_json()
     message = message_info['message']
     chat_info = message['chat']
     chat_id = chat_info['chat_id']
-    text = message['text']
-    data = {'chat_id': chat_id, 'text': text}
-    return data
+    if 'text' in message:
+        text = message['text']
+        data = {'chat_id': chat_id, 'text': text}
+        return data
+    text = 'Я этого не понимаю'
+    send_message(chat_id=chat_id, text=text)
 
 
 def send_message(chat_id: int, text: str) -> None:
-    """Отправка сообщения"""
+    """Function to send a message to the user
+    """
     data = {'chat_id': chat_id, 'text': text}
-    requests.post(url=url+'sendMessage', data=data)
+    requests.post(url=URL+'sendMessage', data=data)
 
 
 def get_a_quote() -> str:
