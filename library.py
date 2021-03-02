84f5eb98e968aeba07145a1afc4f92452aa85b87
GlavRab <alek.bar.ru@yandex.ru>
2021-03-02 21:09:13 +0300

    docs: change docs to functions

--- library.py
+++ library.py
@@ -8,8 +8,7 @@ URL = 'https://api.telegram.org/1666792495:AAGiLEJHfILirO4oLnLfipH2avv4aa9rTv0'
 
 
 def parsing_data() -> dict:
-    """Function to parse a data from user to get a chat id and define text
-    """
+    """Parse a data from user to get a chat id and define text"""
     message_info = request.get_json()
     message = message_info['message']
     chat_info = message['chat']
@@ -23,15 +22,13 @@ def parsing_data() -> dict:
 
 
 def send_message(chat_id: int, text: str) -> None:
-    """Function to send a message to the user
-    """
+    """Send a message to the user"""
     data = {'chat_id': chat_id, 'text': text}
     requests.post(url=URL+'sendMessage', data=data)
 
 
 def get_a_quote() -> str:
-    """Function to get a random quote from a json file
-    """
+    """Get a random quote from a json file"""
     filename = 'quotes.json'
     with open(filename) as file:
         file_with_quotes = json.load(file)
