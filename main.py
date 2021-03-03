7815801f914b654d218d3f286cf59af52ad1e29d
GlavRab <alek.bar.ru@yandex.ru>
2021-03-03 18:28:37 +0300

    feat: make a view function to make server works

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from flask import Flask
+from library import (parsing_data,
+                     send_message,
+                     get_a_quote)
 
 
 app = Flask(__name__)
+
+
+@app.route('/', methods=['POST'])
+def index():
+    if parsing_data():
+        data = parsing_data()
+        chat_id = data['chat_id']
+        text = get_a_quote()
+        send_message(chat_id=chat_id, text=text)
+    return 'not done'
+
 
 if __name__ == '__main__':
     app.run(port=443, ssl_context='adhoc')
